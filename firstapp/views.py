d045bc96127da4dd5f81ec6bbccc71b2cbd5d72b
Aiana Sagynbek <ayana12q34@gmail.com>
2023-10-13 23:01:06 +0600

    request.Get.get

--- firstapp/views.py
+++ firstapp/views.py
@@ -10,9 +10,21 @@ def about(request):
 def contact(request):
     return HttpResponse("<h2>Koнтaкты</h2>")
 
-def products(request, productid = 1):
-    output = "<h2>Продукт- № {0}</h2>".format(productid)
+# def products(request, productid = 1):
+#     output = "<h2>Продукт- № {0}</h2>".format(productid)
+#     return HttpResponse(output)
+
+def products(request, productid):
+    category = request.GET.get("cat", "")
+    output = "<h2>Продукт № {0} Категория: {1}</h2>".format(productid, category)
     return HttpResponse(output)
-def users(request, id, name):
-    output = "<h2>Пользователь</h2><h3>id: {0} Имя:{1}</hЗ>".format(id,name)
+
+# def users(request, id=1, name="Ayane"):
+#     output = "<h2>Пользователь</h2><h3>id: {0} Имя:{1}</hЗ>".format(id,name)
+#     return HttpResponse(output)
+
+def users(request):
+    id = request.GET .get("id", 1)
+    name = request.GET.get("name", "Ayane")
+    output = "<h2>Пользователь</h2><hЗ>id: {0} Имя: {1}</hЗ >".format(id, name)
     return HttpResponse(output)
